67a060884bcaed4fc38bc8cee856ff470b993fa6
IKA TI21f <114545046+ikasalbella@users.noreply.github.com>
2022-11-12 21:47:33 +0700

    PBO6_2

---
+++ Laptop.java
@@ -0,0 +1,13 @@
+package Tugas.PBO6.IKA;
+
+public interface Laptop {
+    
+    int MAX_VOL=100;
+    int MIN_VOL=0;
+    
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
+
